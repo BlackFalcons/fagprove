ff82600aecb3dd4492667a802cb3ab853f7453b8
Eivind <brage211@gmail.com>
2020-06-15 15:57:27 +0200

    ES6 enhancement

--- src/js/presentation-intro.js
+++ src/js/presentation-intro.js
@@ -4,12 +4,14 @@ function presentation_item(control_key, undo_key, control_callback, undo_callbac
     const intro_header = document.querySelector("#introduction-header");
     const about_me = document.querySelector("#task-content");
 
+    const keyPressed = key => {
+        return event.isComposing || event.keyCode === key
+    };
+
     document.addEventListener("keydown", event => {
-        if(event.isComposing || event.keyCode === control_key) {
-            control_callback(about_me, intro_header)
-        } else if (event.isComposing || event.keyCode === undo_key) {
-            undo_callback(intro_header, about_me)
-        }
+        keyPressed(control_key) ? control_callback(about_me, intro_header)
+            : keyPressed(undo_key) ? undo_callback(intro_header, about_me)
+            : false
     });
 }
 
@@ -18,11 +20,11 @@ function hideAndShowItem(show_item_selector, hide_item_selector) {
     show_item_selector.style.display = "block";
 }
 
-presentation_item(13, 32, hideAndShowItem,  hideAndShowItem)
+presentation_item(13, 32, hideAndShowItem, hideAndShowItem)
 
 document.addEventListener("keydown", event => {
     const goToFetchedSite = selector => window.location.href = document.querySelector(selector).getAttribute("href");
-    if(event.isComposing || event.keyCode === 39) {
+    if (event.isComposing || event.keyCode === 39) {
         goToFetchedSite(".presentation-link-right");
     } else if (event.isComposing || event.keyCode === 37) {
         goToFetchedSite(".presentation-link-left");
