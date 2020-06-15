6fdd8731b1a43bb39386dc0f0c93ed1d1348bf2c
Eivind <brage211@gmail.com>
2020-06-15 16:06:29 +0200

    Last commit of the day enhance some ES5 syntax to ES6

--- src/js/presentation-intro.js
+++ src/js/presentation-intro.js
@@ -1,12 +1,13 @@
 "use strict";
 
+const keyPressed = key => {
+    return event.isComposing || event.keyCode === key
+};
+
 function presentation_item(control_key, undo_key, control_callback, undo_callback) {
     const intro_header = document.querySelector("#introduction-header");
-    const about_me = document.querySelector("#task-content");
 
-    const keyPressed = key => {
-        return event.isComposing || event.keyCode === key
-    };
+    const about_me = document.querySelector("#task-content");
 
     document.addEventListener("keydown", event => {
         keyPressed(control_key) ? control_callback(about_me, intro_header)
@@ -24,9 +25,7 @@ presentation_item(13, 32, hideAndShowItem, hideAndShowItem)
 
 document.addEventListener("keydown", event => {
     const goToFetchedSite = selector => window.location.href = document.querySelector(selector).getAttribute("href");
-    if (event.isComposing || event.keyCode === 39) {
-        goToFetchedSite(".presentation-link-right");
-    } else if (event.isComposing || event.keyCode === 37) {
-        goToFetchedSite(".presentation-link-left");
-    }
+    keyPressed(39) ? goToFetchedSite(".presentation-link-right")
+        : keyPressed(37) ? goToFetchedSite(".presentation-link-left")
+        : false;
 });
