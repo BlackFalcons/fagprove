12c86d35a504dc80e5c7859eac7984f0b0b0a3f1
Eivind <brage211@gmail.com>
2020-06-16 08:16:50 +0200

    Add mobile support for displaying presentation

--- src/js/presentation-intro.js
+++ src/js/presentation-intro.js
@@ -1,12 +1,11 @@
 "use strict";
-
+// Checks for a key press
 const keyPressed = (key, e) => {
     return e.isComposing || e.keyCode === key
 };
 function presentation_item(control_key, undo_key, control_callback, undo_callback) {
-    const intro_header = document.querySelector("#introduction-header");
 
-    const about_me = document.querySelector("#task-content");
+const elementExist = element => {
     document.addEventListener("keydown", event => {
         keyPressed(control_key, event) ? control_callback(about_me, intro_header)
             : keyPressed(undo_key, event) ? undo_callback(intro_header, about_me)
@@ -18,7 +17,7 @@ function hideAndShowItem(show_item_selector, hide_item_selector) {
     hide_item_selector.style.display = "none";
     show_item_selector.style.display = "block";
 
-}
+const intro_header = document.querySelector("#introduction-header");
 
 presentation_item(13, 32, hideAndShowItem, hideAndShowItem)
 document.addEventListener("keydown", event => {
@@ -27,3 +26,12 @@ document.addEventListener("keydown", event => {
         : keyPressed(37, event) ? goToFetchedSite(".presentation-link-left")
         : false;
 });
+
+// Add mobile support
+if (window.matchMedia("(max-width: 700px)").matches) {
+    const presentationContent = document.querySelector("#presentation-content");
+    presentationContent.addEventListener("click", event => {
+        getComputedStyle(intro_header).display === "block" ? hideAndShowItem(about_me, intro_header) : hideAndShowItem(intro_header, about_me);
+    })
+
+}
